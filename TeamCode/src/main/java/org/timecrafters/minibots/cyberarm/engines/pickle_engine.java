cf0526acaeccc96d741280fd0a1d58c0c8e7a48d
scottbadger777 <37743855+scottbadger777@users.noreply.github.com>
2022-03-10 20:18:34 -0600

    vexy working

--- TeamCode/src/main/java/org/timecrafters/minibots/cyberarm/engines/pickle_engine.java
+++ TeamCode/src/main/java/org/timecrafters/minibots/cyberarm/engines/pickle_engine.java
@@ -3,19 +3,17 @@ package org.timecrafters.minibots.cyberarm.engines;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.cyberarm.engine.V2.CyberarmEngine;
-import org.timecrafters.minibots.cyberarm.pickle_minibot;
-import org.timecrafters.minibots.cyberarm.states.pickle_telescope_state;
+import org.timecrafters.minibots.cyberarm.pickle_minibot_general;
+import org.timecrafters.minibots.cyberarm.states.pickle_teleop_state;
 
 @TeleOp (name = "pickle_minibot teleop", group = "minibot")
 public class pickle_engine extends CyberarmEngine {
-    pickle_minibot robot;
+    pickle_minibot_general robot;
 
     @Override
     public void setup (){
-        robot = new pickle_minibot(this);
-
-
-        addState(new pickle_telescope_state(robot));
+        robot = new pickle_minibot_general(this);
+        addState(new pickle_teleop_state(robot));
     }
 
 }
