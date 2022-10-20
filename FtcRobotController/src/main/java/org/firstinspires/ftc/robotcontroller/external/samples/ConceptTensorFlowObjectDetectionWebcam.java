4e071679518e48063c0aa497eab265f0b980b472
SpencerPiha <88354753+SpencerPiha@users.noreply.github.com>
2022-10-20 18:49:33 -0500

    Created Autonomous folders

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetectionWebcam.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetectionWebcam.java
@@ -60,13 +60,15 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
      * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
      * Here we assume it's an Asset.    Also see method initTfod() below .
      */
-//    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
-    private static final String TFOD_MODEL_ASSET = "22-23-Custom-Images-V2.tflite";
+    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+//    private static final String TFOD_MODEL_ASSET = "22-23-Custom-Images-V2.tflite";
 
     private static final String[] LABELS = {
-            "Wrench 1",
-            "Screw 2",
-            "Screw Driver 3"
+
+            "1 Bolt",
+            "2 Bulb",
+            "3 Panel"
+
     };
 
     /*
---
+++ TeamCode/src/main/java/org/timecrafters/Autonomous/States/LowerArm.java
@@ -0,0 +1,11 @@
+package org.timecrafters.Autonomous.States;
+
+import org.cyberarm.engine.V2.CyberarmState;
+
+public class LowerArm extends CyberarmState {
+
+    @Override
+    public void exec() {
+
+    }
+}
