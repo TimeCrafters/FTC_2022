69f5a5c7856d113b24ebc70b4619308553781b6e
SpencerPiha <88354753+SpencerPiha@users.noreply.github.com>
2022-11-11 14:52:45 -0600

    camera servo sate added

---
+++ TeamCode/src/main/java/org/timecrafters/Autonomous/States/ServoCameraRotate.java
@@ -0,0 +1,26 @@
+package org.timecrafters.Autonomous.States;
+
+import org.cyberarm.engine.V2.CyberarmState;
+import org.timecrafters.testing.states.PhoenixBot1;
+
+public class ServoCameraRotate extends CyberarmState {
+    private final boolean stateDisabled;
+    PhoenixBot1 robot;
+    private double ServoPosition;
+
+    public ServoCameraRotate(PhoenixBot1 robot, String groupName, String actionName) {
+        this.stateDisabled = !robot.configuration.action(groupName, actionName).enabled;
+        this.robot = robot;
+        this.ServoPosition = robot.configuration.variable(groupName, actionName, "ServoPosition").value();
+
+    }
+    @Override
+    public void exec() {
+        if (stateDisabled) {
+            setHasFinished(true);
+        } else {
+            robot.CameraServo.setPosition(ServoPosition);
+            setHasFinished(true);
+    }
+    }
+}
