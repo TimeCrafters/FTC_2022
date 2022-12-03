2749ede3b9acecce7434f360972787f1befb222e
SpencerPiha <88354753+SpencerPiha@users.noreply.github.com>
2022-12-03 10:43:17 -0600

    Changed tele op

--- TeamCode/src/main/java/org/timecrafters/TeleOp/engine/PhoenixTeleOP.java
+++ TeamCode/src/main/java/org/timecrafters/TeleOp/engine/PhoenixTeleOP.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.cyberarm.engine.V2.CyberarmEngine;
 import org.timecrafters.TeleOp.states.PhoenixBot1;
+import org.timecrafters.TeleOp.states.PhoenixTeleOPState;
 
 @TeleOp (name = "APhoenixTeleOP")
 
@@ -15,7 +16,6 @@ public class PhoenixTeleOP extends CyberarmEngine {
     public void setup() {
 
         robot = new PhoenixBot1(this);
-
-//        addState(new PhoenixTeleOPState(robot));
+        addState(new PhoenixTeleOPState(robot));
     }
 }
