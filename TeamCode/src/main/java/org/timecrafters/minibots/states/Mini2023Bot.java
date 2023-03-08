2d1f930593b96c08c20b6af57b4e3ef5fe21b1cb
NerdyBirdy460 <111399933+NerdyBirdy460@users.noreply.github.com>
2023-03-08 16:07:18 -0600

    Minibot program- day 2

--- TeamCode/src/main/java/org/timecrafters/minibots/states/Mini2023Bot.java
+++ TeamCode/src/main/java/org/timecrafters/minibots/states/Mini2023Bot.java
@@ -11,8 +11,8 @@ public class Mini2023Bot {
     private final Mini2023Engine engine;
     public TimeCraftersConfiguration configuration;
 
-        public DcMotor Opportunity, Spirit, Ingenuity;
-        public CRServo servoA, servoB, servoC;
+        public DcMotor Opportunity, Spirit, Victoria, Endeavour; //Don't ask. Just don't.
+        public CRServo servoA, servoB, servoC; //Just be thankful the servos have 'normal' names.
 
     public Mini2023Bot(Mini2023Engine engine) {
         this.engine = engine;
@@ -23,6 +23,8 @@ public class Mini2023Bot {
 
         Opportunity = engine.hardwareMap.get(DcMotor.class, "Left Wheel");
         Spirit = engine.hardwareMap.get(DcMotor.class, "Right Wheel");
+//        Victoria = engine.hardwareMap.get(DcMotor.class, "Rear Dead Wheel");
+//        Endeavour = engine.hardwareMap.get(DcMotor.class, "Front Dead Wheel");
 
         servoA = engine.hardwareMap.get(CRServo.class, "Servo 1");
         servoB = engine.hardwareMap.get(CRServo.class, "Servo 2");
--- TeamCode/src/main/java/org/timecrafters/minibots/states/Mini2023State.java
+++ TeamCode/src/main/java/org/timecrafters/minibots/states/Mini2023State.java
@@ -1,38 +1,54 @@
 package org.timecrafters.minibots.states;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.cyberarm.engine.V2.CyberarmState;
-import org.timecrafters.minibots.engines.Mini2023Engine;
 
 public class Mini2023State extends CyberarmState {
     private final Mini2023Bot robot;
 
-    public double driveSpeed, orbitSpeed;
+    public double lThrust, rThrust, orbitSpeed;
 
     public Mini2023State(Mini2023Bot robot) {this.robot = robot;}
 
 @Override
 public void init() {
-    driveSpeed = 0;
+    lThrust = 0;
+    rThrust = 0;
     orbitSpeed = 0;
     robot.servoA.setPower(0);
     robot.servoB.setPower(0);
     robot.servoC.setPower(0);
-    robot.Opportunity.setPower(driveSpeed);
-    robot.Spirit.setPower(driveSpeed);
-
+    robot.Opportunity.setPower(lThrust);
+    robot.Spirit.setPower(rThrust);
 }
 
 @Override
 public void exec() {
 
-    if (Math.abs(engine.gamepad1.left_stick_y) > 0.1 && Math.abs(engine.gamepad1.left_stick_x) < 0.1) {
-        orbitSpeed = engine.gamepad2.left_stick_y;
-        robot.Opportunity.setPower(driveSpeed);
-        robot.Spirit.setPower(driveSpeed);
+    if (Math.abs(engine.gamepad1.left_stick_y) > 0.1) {
+        lThrust = engine.gamepad2.left_stick_y;
+        robot.Opportunity.setPower(lThrust);
+    } else {
+        lThrust = 0;
+        robot.Opportunity.setPower(lThrust);
     }
 
-    if (Math.abs(engine.gamepad1.right_stick_x) > 0.1) {
-        orbitSpeed = engine.gamepad1.right_stick_x * 0.75;
+    if (Math.abs(engine.gamepad1.right_stick_y) > 0.1) {
+        rThrust = engine.gamepad1.right_stick_y;
+        robot.Spirit.setPower(rThrust);
+    } else {
+        rThrust = 0;
+        robot.Spirit.setPower(rThrust);
+    }
+
+    if (engine.gamepad1.right_trigger > 0.1) {
+        orbitSpeed = engine.gamepad1.right_trigger * 0.75;
+        robot.servoA.setPower(orbitSpeed);
+        robot.servoB.setPower(orbitSpeed);
+        robot.servoC.setPower(orbitSpeed);
+    } else if (engine.gamepad1.left_trigger > 0.1) {
+        orbitSpeed = engine.gamepad1.left_trigger * 0.75;
         robot.servoA.setPower(orbitSpeed);
         robot.servoB.setPower(orbitSpeed);
         robot.servoC.setPower(orbitSpeed);
